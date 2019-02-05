73633fc066f20036e06bfedd3257130d005bd352
nivethitha939 <37238449+nivethitha939@users.noreply.github.com>
2019-02-05 10:16:21 +0530

    Create arthimatic.py

---
+++ arthimatic.py
@@ -0,0 +1,16 @@
+a=int(input("enter the frst no:"))
+b=int(input("enter the second no:"))
+print("1.add")
+print("2.subtract")
+print("3.multiplicaton")
+print("4.division")
+choice=int(input("enter ur choice:"))
+if(choice==1):
+    print(a+b)
+elif(choice==2):
+    print(a-b)
+elif(choice==3):
+    print(a*b)
+elif(choice==4):
+    print(a/b)
+    
