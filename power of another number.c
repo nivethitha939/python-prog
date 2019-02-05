4c5020dde146bb8456bbe95553cae090a3c609f8
nivethitha939 <37238449+nivethitha939@users.noreply.github.com>
2019-02-05 14:52:54 +0530

    Create power of another number.c

---
+++ power of another number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b;
+    clrscr();
+    printf("");
+    scanf("%d%d",&a,&b);
+    if(a%b==0)
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+}
