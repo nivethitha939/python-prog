3ae36a9a06b55dcb14f9784a85d6eb6288ce822c
nivethitha939 <37238449+nivethitha939@users.noreply.github.com>
2019-02-05 12:35:07 +0530

    Create no with in the range.c

---
+++ no with in the range.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void)
+{
+	int n;
+	scanf("%d",&n);
+	int i;
+	for(i=1;i<=10;i++)
+	{
+		if(i==n)
+		{
+			printf("\n yes");
+		}
+	}
+	
+	return 0;
+}
