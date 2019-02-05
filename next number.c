1b26ec127f49089b9105327ac3b643c9deb812e7
nivethitha939 <37238449+nivethitha939@users.noreply.github.com>
2019-02-05 12:52:02 +0530

    Create next number.c

---
+++ next number.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conoi.h>
+            void main()
+            {
+            	int n=10,s,i;
+            	for(i=0;i<n+1;i++)
+            	{
+            
+            	printf("%d",i);
+            	}
+            }
+     
