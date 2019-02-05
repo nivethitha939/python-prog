cbe4a9a2f8f77e428abf1542eb87a6581f272eb1
nivethitha939 <37238449+nivethitha939@users.noreply.github.com>
2019-02-05 11:12:34 +0530

    Update compare two string.c

--- compare two string.c
+++ compare two string.c
@@ -4,9 +4,9 @@ int main(void)
 {
 	char str1[50],str2[50];
 	int n1,n2;
-	printf("enter the string1:");
+	printf("");
 	scanf("%s",str1);
-	printf("enter the string2:");
+	printf("");
 	scanf("%s",str2);
 	n1=strlen(str1);
 	n2=strlen(str2);
