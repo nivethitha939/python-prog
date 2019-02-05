f7fd844d4a73a730414a4ed522f1714809819770
nivethitha939 <37238449+nivethitha939@users.noreply.github.com>
2019-02-05 10:06:24 +0530

    Create up to lower.c

---
+++ up to lower.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void) 
+{
+	char str[10];
+	int i;
+	scanf("%s",str);
+	for(i=0;i<=10;i++)
+	{
+	if((str[i]>='a')&&(str[i]<='z'))
+	{
+		str[i]=str[i]-32;
+	}
+	else
+	{
+		str[i]=str[i]+32;
+	}
+	}
+	printf("%s",str);
+	
+	return 0;
+}
