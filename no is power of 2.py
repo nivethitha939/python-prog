277d5aa830cab76841d5e7998f82749cbcfe34a0
nivethitha939 <37238449+nivethitha939@users.noreply.github.com>
2019-02-05 15:11:34 +0530

    Create no is power of 2.py

---
+++ no is power of 2.py
@@ -0,0 +1,13 @@
+def is_power_of_two(n):
+    if n <= 0:
+        return False
+    else:
+        return n & (n - 1) == 0
+ 
+ 
+n = int(input(""))
+ 
+if is_power_of_two(n):
+    print("yes".format(n))
+else:
+    print("no".format(n))
